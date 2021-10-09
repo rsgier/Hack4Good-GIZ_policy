0df89c6b8e59c7b99b43b6067c0e99be0e4950cf
Jonathan Doorn <jdoorn@purdue.edu>
2021-10-09 14:38:10 +0200

    documented textimporter

--- textutils/importer.py
+++ textutils/importer.py
@@ -1,10 +1,24 @@
+"""Assists in importing text files that may contain unknown characters
 
-import nltk, codecs
+Some files produced by OCR engines may contain invalid UTF-8 and ASCII
+characters that are not compatible with NLP libraries. This class will
+import a text file safely and provide methods for accessing the text.
+
+    Typical usage example:
+
+    report = TextImporter("annualreport.txt")
+    tokens = report.tokenize_text()
+
+"""
+
+import nltk
+import codecs
+
 
 class TextImporter:
 
     def __init__(self, path) -> None:
-        with codecs.open(path, errors='ignore', encoding="utf8") as f:
+        with codecs.open(path, errors="ignore", encoding="utf8") as f:
             self.text = f.read()
 
     def tokenize_text(self):
