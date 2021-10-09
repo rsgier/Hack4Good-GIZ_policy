5cba9e8e550fdcdbe2c1985e9423d4b5978f49b2
Jonathan Doorn <jdoorn@purdue.edu>
2021-10-09 15:05:27 +0200

    Importer to main

--- testtext.py
+++
@@ -1,9 +0,0 @@
-'''
-This module  a docstring now
-'''
-import nltk
-from textutils.importer import TextImporter
-
-nltk.download('punkt')
-
-print(TextImporter('texts/AnnualReport2017-2018.txt').tokenize_text())
