8e91a6c1fc285339709ee9528567622395b64fdd
Jonathan Doorn <jdoorn@purdue.edu>
2021-11-11 17:24:04 +0100

    simplified ngram calculation, but ngrams on a large doc takes much too long

--- code/n_gram_correlation.py
+++ code/n_gram_correlation.py
@@ -2,26 +2,18 @@ import spacy
 from spacy.tokens import Span, Doc
 # Can attach a special getter to a general span that will calculate the correlation to a group of keywords
 # Later on can make span groups based on the value of the correlation values.
-from correlation import KeywordCorrelator
+from correlation import SpanCorrelator
 
 
 class NGramCorrelateSpacy:
 
-    def __init__(self, keywords, n_gram_length, threshold) -> None:
-        self.correlator = KeywordCorrelator(keywords)
-        self.n_gram_length = n_gram_length
-        self.threshold = threshold
+    def __init__(self, keywords, threshold, tag) -> None:
+        self.correlator = SpanCorrelator(keywords, threshold, tag)
 
     def correlate_spans(self, doc: Doc, n_gram_size):
-        span_correlate = lambda span: self.correlator(span.text)
         n_gram_tuples = self.get_n_gram_tuples(n_gram_size, len(doc))
         span_candidates = [doc[t[0]:t[1]] for t in n_gram_tuples]
-        relevance_coefficients = self.correlator(span_candidates)
-
-        span_candidates = [
-            s for s, c in zip(span_candidates, relevance_coefficients)
-            if c > self.threshold
-        ]
+        [self.correlator(doc, c) for c in span_candidates]
 
     def get_n_gram_tuples(self, size, doc_len):
         return [(i, i + size) for i in range(doc_len - size + 1)]
